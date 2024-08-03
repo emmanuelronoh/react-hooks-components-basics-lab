005e8d7ec477e45c0cc8e25e57c3a3511939cb99
emmanuel ronoh <eronoh036@gmail.com>
2024-08-03 17:55:20 +0300

    first commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,30 +1,45 @@
-import React from "react";
+// import React from "react";
 
-function NavBar() {
-  return (
-    <nav>
-      <a href="#home">I'm a link!</a>
-    </nav>
-  );
-}
+// function NavBar() {
+//   return (
+//     <nav>
+//       <a href="#home">I'm a link!</a>
+//     </nav>
+//   );
+// }
 
-function Home() {
-  return (
-    <div id="home">
-      <h1>Home</h1>
-    </div>
-  );
-}
+// function Home() {
+//   return (
+//     <div id="home">
+//       <h1>Home</h1>
+//     </div>
+//   );
+// }
 
-{/* write an <About> component here */}
+// {/* write an <About> component here */}
 
+
+// function App() {
+//   return (
+//     <div>
+//       <NavBar />
+//       {/* add the <Home> component here */}
+//       {/* add your <About> component here */}
+//     </div>
+//   );
+// }
+
+// export default App;
+// App.js
+import React from 'react';
 
 function App() {
   return (
-    <div>
-      <NavBar />
-      {/* add the <Home> component here */}
-      {/* add your <About> component here */}
+    <div className="App">
+      <nav>
+        <div id="home">Home</div>
+        <div id="about">About</div>
+      </nav>
     </div>
   );
 }
